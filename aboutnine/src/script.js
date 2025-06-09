bc8279f389a852f2f320e3e8170150fefaebc796
Sicknine <156204309+SNaytiP@users.noreply.github.com>
2025-06-09 20:39:42 +0700

    09/06/2025
    build portfolio section

--- aboutnine/src/script.js
+++ aboutnine/src/script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Website loaded successfully!');
   });
+
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+  });
